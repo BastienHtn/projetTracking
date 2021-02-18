1ed26d16875dbcc194fbc3975a66847062f3cc91
BastienHtn <bastien.haton@gmail.com>
2021-02-18 03:56:19 +0100

    fix routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { IndexComponent } from './index/index.component';
 import { ChampionStatsComponent } from './champion-stats/champion-stats.component';
-import { ChampionsWikiComponent } from './champions-wiki/champions-wiki.component';
+import { ChampionsInfosComponent } from './champions-infos/champions-infos.component';
 
 const routes: Routes = [
   { path: 'championStats', component: ChampionStatsComponent },
   { path: 'home', component: IndexComponent },
-  { path: 'championsInfos', component: ChampionsWikiComponent },
+  { path: 'championsInfos', component: ChampionsInfosComponent },
   { path: '**', redirectTo: 'home', pathMatch: 'full' },
   
 ];
