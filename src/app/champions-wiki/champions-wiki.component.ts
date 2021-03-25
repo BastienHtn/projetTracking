520d204e34ada4068cb4fb295beab303b7ec6199
BastienHtn <bastien.haton@gmail.com>
2021-03-25 19:31:20 +0100

    another merge fix

--- src/app/champions-wiki/champions-wiki.component.ts
+++ src/app/champions-wiki/champions-wiki.component.ts
@@ -1,12 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-<<<<<<< HEAD
 import { ChampionDTO } from '../_json/champion';
 import * as Champions from '../../assets/dataDragon/11.6.1/data/fr_FR/champion.json';
 import { RiotApiService } from '../riot-api.service';
 import { Router, ActivatedRoute } from '@angular/router';
 import { retry } from 'rxjs/operators';
-=======
->>>>>>> dc3e64a5045a722470e18d03e616de5962cf6afd
+
 
 @Component({
   selector: 'app-champions-wiki',
@@ -18,36 +16,25 @@ export class ChampionsWikiComponent implements OnInit {
   arrayListChampions: Array<ChampionDTO> = [];
   isDataObtained: boolean = false;
 
-<<<<<<< HEAD
   constructor(
     public route: ActivatedRoute,
     public riotApiService: RiotApiService,
     public router: Router
-  ) {
-=======
-  constructor() {
->>>>>>> dc3e64a5045a722470e18d03e616de5962cf6afd
-   }
+  ) {}
   
 
   ngOnInit() {
-  
-  try{
-    for(var i in this.listChampions){
-      this.arrayListChampions.push(this.listChampions[i]);
+    try{
+      for(var i in this.listChampions){
+        this.arrayListChampions.push(this.listChampions[i]);
+      }
+      
+      this.isDataObtained = true;
+    }catch{
+      this.isDataObtained = false;
+      console.log("Erreur d'implementation de liste");
     }
-    
-    this.isDataObtained = true;
-  }catch{
-    this.isDataObtained = false;
-    console.log("Erreur d'implementation de liste");
   }
-
-<<<<<<< HEAD
-  }
-
-=======
->>>>>>> dc3e64a5045a722470e18d03e616de5962cf6afd
   
 
 }
